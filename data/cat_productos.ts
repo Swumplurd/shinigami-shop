8746c31025ea5a169fbef4ac11c03e4ef855cb3c
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-11-01 00:51:09 -0600

    mejoradas categorias

--- data/cat_productos.ts
+++ data/cat_productos.ts
@@ -4,105 +4,105 @@ export const cat_productos: CategoriasProductos[] = [
     {
         name: "Mangas y Libros",
         desc: "Encuentra tus mangas y libros favoritos al mejor precio",
-        img: "/img/cats/cat1.jpeg",
+        img: "/img/cats/mangas-libros.png",
         href: "mangas-libros",
         kw: "mangas, mangas anime libros, one piece, demon slayer, dr stone, jojos, ataque de los titanes, naruto, sword art online, chainsaw man, death note, jujutsu kaisen, my hero academia, tokyo revangers"
     },
     {
         name: "Articulos por Temporada",
         desc: "Encuentra esos articulos otaku que estabas buscando ideales para tu temporada del año favorita",
-        img: "/img/cats/cat2.jpeg",
+        img: "/img/cats/temporada.png",
         href: "temporada",
         kw: "halloween, san valentin, navidad, articulos otaku, articulos de anime, anime, cosplay"
     },
     {
         name: "Escolares y Papeleria",
         desc: "Encuentra todos los articulos escolares y de papelera personalizados con tus personajes de anime favoritos",
-        img: "/img/cats/cat4.jpeg",
+        img: "/img/cats/escolar-papeleria.png",
         href: "escolar-papeleria",
         kw: "cuadernos anime, cuadernos goku, lapiceros, articulos escolares personalizados, articulos escolares de anime"
     },
     {
         name: "Ropa",
         desc: "Encuentra gran varriedad de ropa con estampados y bordados de tus personajes de anime o peliculas favoritos",
-        img: "/img/cats/cat5.jpeg",
+        img: "/img/cats/ropa.png",
         href: "ropa",
         kw: "playeras, pantalones, sudaderas, playera, pantalon, sudadera, unisex, gorras, playeras con diseño, playeras de anime"
     },
     {
         name: "Joyas y complementos",
         desc: "Encuentra la mejor joyeria y complementos que usan tus personajes favoritos",
-        img: "/img/cats/cat7.jpeg",
+        img: "/img/cats/joyas-complementos.png",
         href: "joyas-complementos",
         kw: "aretes, collares, pulseras, gargantillas, joyeria de anime, complementos de anime, cosplay complementos"
     },
     {
-        name: "Merchadising",
+        name: "Merchandising",
         desc: "Encuentra todo tipo de mercancia como: mochilas, carteras, electronicos, vasos, tazas, llaveros, gorras, bolsas, etc.",
-        img: "/img/cats/cat11.jpeg",
+        img: "/img/cats/merchandising.png",
         href: "merchandising",
         kw: "mochilas, carteras, electronicos, vasos, tazas, llaveros, gorras, bolsas, articulos personalizados de anime"
     },
     {
         name: "Peluches",
         desc: "Encuentra todos los peluches tus personajes favoritos",
-        img: "/img/cats/cat6.jpeg",
+        img: "/img/cats/peluches.png",
         href: "peluches",
         kw: "peluches, juguetes, personajes peluches, personaje anime peluche, lufi peluche, nezuko peluche, demon slayer peluche"
     },
     {
         name: "Juegos",
         desc: "Encuentra todo tipo de videojuegos y tambien juegos de mesa",
-        img: "/img/cats/cat9.jpeg",
+        img: "/img/cats/juegos.png",
         href: "juegos",
         kw: "juegos de xbox, juegos de play, juegos para pc, juegos de dragon ball, juegos de mesa, juegos divertidos, juegos varatos"
     },
     {
         name: "Cosplays",
         desc: "Encuentra los mejores trajes de cosplay para que vistas como los personajes de tu anime favorito",
-        img: "/img/cats/cat8.jpeg",
+        img: "/img/cats/cosplays.png",
         href: "cosplays",
         kw: "cosplay, cosplays, venta de cosplays, venta de articulos para cosplay"
     },
     {
         name: "Figuras",
         desc: "Encuentra gran variedad de figuras a escala de tus personajes favoritos",
-        img: "/img/cats/cat14.jpeg",
+        img: "/img/cats/figuras.png",
         href: "figuras",
         kw: "figuras coleccionables, figuras de resina, figuras de personajes, funko pop, funko, figuras a escala, figuras de waifus, figuras de anime"
     },
     {
         name: "Edredones y Cobijas",
         desc: "Encuentra cobijas y edredones con diseños de tus animes o peliculas favoritos",
-        img: "/img/cats/cat3.jpeg",
+        img: "/img/cats/edredones-cobijas.png",
         href: "edredones-cobijas",
         kw: "edredones, cobijas, cobijas con diseño, cobijas de anime, edredones de anime"
     },
     {
         name: "Posters",
         desc: "Aqui encontraras posters con los cuales puedes adornar tu cuarto o tu setuup con tus personajes favoritos",
-        img: "/img/cats/cat10.jpeg",
+        img: "/img/cats/posters.png",
         href: "posters",
         kw: "posters, posters de personajes de anime, posters de peliculas, posters de series, poster de nezuko, poster de goku, poster de dragon ball"
     },
     {
         name: "DVDs y Blue-rays",
         desc: "Encuentra tus animes o peliculas favoritas en diferentes formatos de video",
-        img: "/img/cats/cat12.jpeg",
+        img: "/img/cats/dvd-blueray.png",
         href: "dvd-blueray",
         kw: "dvd anime, blue ray de anime, dvd, blue ray, one piece, demon slayer, ataque de los titanes"
     },
     {
         name: "Varitas y Espadas",
         desc: "Encuentra gran variedad de utileria tales como varitas de Harry Potter y espadas de tus series, animes y peliculas favoritas",
-        img: "/img/cats/cat13.jpeg",
+        img: "/img/cats/varitas-espadas.png",
         href: "varitas-espadas",
         kw: "varitas, espadas, varitas de harry potter, estada de el señor de los anillos, katanas, replica de katana, replica de espada, replica de varita de harry potter"
     },
     {
         name: "Comida",
         desc: "Encuentra comida internacional, dulces, snacks, etc",
-        img: "/img/cats/cat15.jpeg",
+        img: "/img/cats/comida.png",
         href: "comida",
         kw: "dulces japoneses, snacks japoneses, comida japonesa"
     },
