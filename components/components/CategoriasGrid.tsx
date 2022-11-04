7625532cd3cb8b18a16f246ef01ae4b3f4c3672b
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-11-04 06:12:39 -0600

    corregida key en grid container

--- components/components/CategoriasGrid.tsx
+++ components/components/CategoriasGrid.tsx
@@ -6,7 +6,7 @@ export const CategoriasGrid = () => {
   return (
     <Grid.Container gap={2} justify="center">
       {cat_productos.map((cat, index) => (
-        <CategoriasGridItem cat={cat} index={index}/>
+        <CategoriasGridItem key={index} cat={cat} index={index}/>
       ))}
     </Grid.Container>
   );
